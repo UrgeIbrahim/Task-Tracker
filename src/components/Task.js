bcb2bee1c5cbe23630277e8e8e4ca9fbecf3a6cc
UrgeIbrahim <urgee.hassanee15@gmail.com>
2021-12-10 22:54:06 -0500

    Complete version

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,33 @@
 import { FaTimes } from 'react-icons/fa'
 
+
+
+
 const Task = ({ task, onDelete, onToggle }) => {
+    function handleParentClick () { 
+        // console.log(e)
+        onToggle(task.id);
+      }
+    
+    function handleChildClick (e) {
+        e.stopPropagation();
+        onDelete(task.id)
+    }
+
+    
     return (
         <div
-            className={`task ${task.reminder ? 'reminder' : ''}`}
-            onClick={() => onToggle(task.id)}
+            className={`task ${task.reminder ? 
+            'reminder' : ''}`}
+            onClick={handleParentClick}
+            // ={() => onToggle(task.id)}
         >
             <h3>
                 {task.text}
                 <FaTimes
                     style={{ color: 'red', pointer: 'cursor' }}
-                    onClick={() => onDelete(task.id)}
+                    onClick={handleChildClick}
+                    // onClick={() => onDelete(task.id)}
                 />
             </h3>
             <p>{task.day}</p>
